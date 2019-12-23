3252a94b4a7051ba53e23473342acc5568977207
Jason Zhang <zjjc123@hotmail.com>
2019-12-22 22:52:14 -0800

    Basic functioning app
    need to get rid of pop up

--- app/src/main/java/com/jgr/camerafun/MainActivity.java
+++ app/src/main/java/com/jgr/camerafun/MainActivity.java
@@ -2,11 +2,18 @@ package com.jgr.camerafun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +21,18 @@ public class MainActivity extends AppCompatActivity {
     TextView input;
     int time = 0;
 
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
 
         submitButton = findViewById(R.id.setButton);
         input = findViewById(R.id.timeInput);
@@ -28,13 +43,46 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     time = Integer.parseInt(input.getText().toString());
                     Toast.makeText(getApplicationContext(), "Timer: " + time, Toast.LENGTH_LONG).show();
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     Toast.makeText(getApplicationContext(), "Error! " + input.getText().toString(), Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("CameraFun","OnPause()");
+        new CountDownTimer(time * 1000, 1000) {
+            public void onFinish() {
+                openFrontFacingCamera();
+            }
+
+            public void onTick(long millisUntilFinished) {
+                // millisUntilFinished    The amount of time until finished.
+            }
+        }.start();
+
+    }
+
+    private Camera openFrontFacingCamera()
+    {
+        int cameraCount = 0;
+        Camera cam = null;
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        cameraCount = Camera.getNumberOfCameras();
+        for ( int camIdx = 0; camIdx < cameraCount; camIdx++ ) {
+            Camera.getCameraInfo( camIdx, cameraInfo );
+            if ( cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT  ) {
+                try {
+                    cam = Camera.open( camIdx );
+                } catch (RuntimeException e) {
+                    Log.e("CameraFun", "Camera failed to open: " + e.getLocalizedMessage());
+                }
+            }
+        }
+
+        return cam;
+    }
 }
